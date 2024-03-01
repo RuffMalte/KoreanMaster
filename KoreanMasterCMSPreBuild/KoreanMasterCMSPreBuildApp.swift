b123a12b7eab8a05a8ab057ac455aa4b4ca42588
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2024-03-01 22:03:55 +0100

    Initial Commit

---
+++ KoreanMasterCMSPreBuild/KoreanMasterCMSPreBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreanMasterCMSPreBuildApp.swift
+//  KoreanMasterCMSPreBuild
+//
+//  Created by Malte Ruff on 01.03.24.
+//
+
+import SwiftUI
+
+@main
+struct KoreanMasterCMSPreBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
